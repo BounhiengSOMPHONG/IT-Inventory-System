622f34381492735788a6b6d07d35bdcb87a26026
bounhieng <Teng.ios123@gmail.com>
2025-09-23 08:44:34 +0700

    see log for admin

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -2,16 +2,17 @@ const express = require('express');
 const router = express.Router();
 const ProductController = require('../controllers/product.controller');
 const authMiddleware = require('../middleware/auth.middleware');
+const adminMiddleware = require('../middleware/admin.middleware');
 
 
 router.post('/', ProductController.create);
 router.get('/', ProductController.list);
-router.get('/deleted', authMiddleware, ProductController.getDeleted);
-router.get('/edit-logs', ProductController.searchEditLogs);
+router.get('/deleted', adminMiddleware, ProductController.getDeleted);
+router.get('/edit-logs', adminMiddleware, ProductController.searchEditLogs);
 router.get('/:id', ProductController.get);
-router.get('/:id/edit-logs', ProductController.getEditLogs);
+router.get('/:id/edit-logs', adminMiddleware, ProductController.getEditLogs);
 router.put('/:id', ProductController.update);
 router.delete('/:id', ProductController.delete);
-router.patch('/:id/restore', authMiddleware, ProductController.restore);
+router.patch('/:id/restore', adminMiddleware, ProductController.restore);
 
 module.exports = router;
