7d1bdc20ee6fb6bc044da7ee59ffe4aa03dd26f3
bounhieng <Teng.ios123@gmail.com>
2025-09-30 09:27:00 +0700

    up route

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,9 @@ use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
